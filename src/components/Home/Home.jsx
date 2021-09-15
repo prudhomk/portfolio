7ed3454120d539a4cbf4c1d104555860661f32f3
Kalan Prudhomme <cleaver9@gmail.com>
2021-09-15 15:08:43 -0700

    more styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import PublicIcon from '@material-ui/icons/Public';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import styles from './Home.css';
 
@@ -18,7 +18,7 @@ export default function Home() {
             <img src="./Chordial.png"/>
             <br/>
             <a href="https://github.com/Chordial-Music/Chordial-FE" target="_blank" rel="noreferrer"><GitHubIcon className="icon" /></a>
-            <a href="https://chordial.netlify.app/" target="_blank" rel="noreferrer"><LinkedInIcon className="icon" /></a>
+            <a href="https://chordial.netlify.app/" target="_blank" rel="noreferrer"><PublicIcon className="icon" /></a>
             <figcaption>Chordial</figcaption>
             <p>Chordial lets users create songs by offering chord patterns based on harmony and chordal theory. Enjoy the soothing visuals as you effortlessly write music!</p>
           </figure>
@@ -26,7 +26,7 @@ export default function Home() {
             <img src="./Web_Wizard.png"/>
             <br/>
             <a href="https://github.com/KAKMAV/web-wizard" target="_blank" rel="noreferrer"><GitHubIcon className="icon" /></a>
-            <a href="https://addons.mozilla.org/en-US/firefox/addon/web-wizard/" target="_blank" rel="noreferrer"><LinkedInIcon className="icon" /></a>
+            <a href="https://addons.mozilla.org/en-US/firefox/addon/web-wizard/" target="_blank" rel="noreferrer"><PublicIcon className="icon" /></a>
             <figcaption>WebWizard</figcaption>
             <p>Design the web your way with Web Wizard! A Firefox extension that puts CSS design in the user's hands.</p>
           </figure>
@@ -34,7 +34,7 @@ export default function Home() {
             <img src="./TuneMatch.png"/>
             <br/>
             <a href="https://github.com/NC2K/tune-match" target="_blank" rel="noreferrer"><GitHubIcon className="icon" /></a>
-            <a href="https://elated-brattain-3eaa48.netlify.app/" target="_blank" rel="noreferrer"><LinkedInIcon className="icon" /></a>
+            <a href="https://elated-brattain-3eaa48.netlify.app/" target="_blank" rel="noreferrer"><PublicIcon className="icon" /></a>
             <figcaption>Tune Match</figcaption>
             <p>Tune Match utilizes the iTunes API to output randomized top hits to test your music knowledge throughout the decades! Users have control on when the timer starts and can playback collections previous played.</p>
           </figure>
@@ -42,7 +42,7 @@ export default function Home() {
             <img src="./IoSpio.png"/>
             <br/>
             <a href="https://github.com/Gaka-Daka/io-spio" target="_blank" rel="noreferrer"><GitHubIcon className="icon" /></a>
-            <a href="https://gaka-daka.github.io/io-spio/" target="_blank" rel="noreferrer"><LinkedInIcon className="icon" /></a>
+            <a href="https://gaka-daka.github.io/io-spio/" target="_blank" rel="noreferrer"><PublicIcon className="icon" /></a>
             <figcaption>Io Spio</figcaption>
             <p>A modern version of "I-Spy", a hidden object game application. Created in HTML, CSS, and vanilla JavaScript within a 4-day sprint. </p>
           </figure>
