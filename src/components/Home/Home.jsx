d59c02f223d7aafe81404b3b9c2073d21a02f3f9
Kalan Prudhomme <cleaver9@gmail.com>
2021-10-06 11:53:56 -0700

    further styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom';
 import styles from './Title.css';
 import bodystyles from './Body.css';
 import buttonstyles from './Button.css';
+import socialstyles from './Social.css';
 import PublicIcon from '@material-ui/icons/Public';
 import GitHubIcon from '@material-ui/icons/GitHub';
 
@@ -27,7 +28,24 @@ export default function Home() {
           <h1><span>"KALAN</span><span>PRUDHOMME"</span></h1>
         </div>
 
-        <article>
+        <div className={socialstyles.social}>
+          <div className={socialstyles.wrapper}>
+            <div className={`${socialstyles.icon} ${socialstyles.twitter}`}>
+              <div className={socialstyles.tooltip}>Twitter</div>
+              <span><img className={socialstyles.twitter} src="../../../public/twitter.svg"></img></span>
+            </div>
+            <div className={`${socialstyles.icon} ${socialstyles.github}`}>
+              <div className={socialstyles.tooltip}>Github</div>
+              <span><img className={socialstyles.github}></img></span>
+            </div>
+            <div className={`${socialstyles.icon} ${socialstyles.linkedin}`}>
+              <div className={socialstyles.tooltip}>LinkedIn</div>
+              <span><img className={socialstyles.linkedin}></img></span>
+            </div>
+          </div>
+        </div>
+
+        <div className={bodystyles.bio}>
           <img src="funny picture"/>
           <p>A kid who never grew up, 
             always excited to learn something new, 
@@ -35,7 +53,7 @@ export default function Home() {
             loves coming up with build ideas and 
             solving problems that come his way.
           </p>
-        </article>
+        </div>
         <div>
           <ul>
             <h2>Projects</h2>
@@ -45,8 +63,10 @@ export default function Home() {
               <img src="./Chordial.png"/>
               <div>
                 <h3>Chordial</h3>
-                <a href="https://github.com/Chordial-Music/Chordial-FE" target="_blank" rel="noreferrer"><GitHubIcon className="icon" /></a>
-                <a href="https://chordial.netlify.app/" target="_blank" rel="noreferrer"><PublicIcon className="icon" /></a>
+                <div className={bodystyles.links}>
+                  <a href="https://github.com/Chordial-Music/Chordial-FE" target="_blank" rel="noreferrer"><GitHubIcon className="icon" /></a>
+                  <a href="https://chordial.netlify.app/" target="_blank" rel="noreferrer"><PublicIcon className="icon" /></a>
+                </div>
               </div>
               <p>Chordial lets users create songs by offering chord patterns based on harmony and chordal theory. Enjoy the soothing visuals as you effortlessly write music!</p>
             </li>
@@ -55,8 +75,10 @@ export default function Home() {
               <img src="./Web_Wizard.png"/>
               <div>
                 <h3>WebWizard</h3>
-                <a href="https://github.com/KAKMAV/web-wizard" target="_blank" rel="noreferrer"><GitHubIcon className="icon" /></a>
-                <a href="https://addons.mozilla.org/en-US/firefox/addon/web-wizard/" target="_blank" rel="noreferrer"><PublicIcon className="icon" /></a>
+                <div className={bodystyles.links}>
+                  <a href="https://github.com/KAKMAV/web-wizard" target="_blank" rel="noreferrer"><GitHubIcon className="icon" /></a>
+                  <a href="https://addons.mozilla.org/en-US/firefox/addon/web-wizard/" target="_blank" rel="noreferrer"><PublicIcon className="icon" /></a>
+                </div>
               </div>
               <p>Design the web your way with Web Wizard! A Firefox extension that puts CSS design in the user's hands.</p>
             </li>
@@ -64,8 +86,10 @@ export default function Home() {
               <img src="./TuneMatch.png"/>
               <div>
                 <h3>Tune Match</h3>
-                <a href="https://github.com/NC2K/tune-match" target="_blank" rel="noreferrer"><GitHubIcon className="icon" /></a>
-                <a href="https://elated-brattain-3eaa48.netlify.app/" target="_blank" rel="noreferrer"><PublicIcon className="icon" /></a>
+                <div className={bodystyles.links}>
+                  <a href="https://github.com/NC2K/tune-match" target="_blank" rel="noreferrer"><GitHubIcon className="icon" /></a>
+                  <a href="https://elated-brattain-3eaa48.netlify.app/" target="_blank" rel="noreferrer"><PublicIcon className="icon" /></a>
+                </div>
               </div>
               <p>Tune Match utilizes the iTunes API to output randomized top hits to test your music knowledge throughout the decades! Users have control on when the timer starts and can playback collections previous played.</p>
             </li>
@@ -73,8 +97,10 @@ export default function Home() {
               <img src="./IoSpio.png"/>
               <div>
                 <h3>Io Spio</h3>
-                <a href="https://github.com/Gaka-Daka/io-spio" target="_blank" rel="noreferrer"><GitHubIcon className="icon" /></a>
-                <a href="https://gaka-daka.github.io/io-spio/" target="_blank" rel="noreferrer"><PublicIcon className="icon" /></a>
+                <div className={bodystyles.links}>
+                  <a href="https://github.com/Gaka-Daka/io-spio" target="_blank" rel="noreferrer"><GitHubIcon className="icon" /></a>
+                  <a href="https://gaka-daka.github.io/io-spio/" target="_blank" rel="noreferrer"><PublicIcon className="icon" /></a>
+                </div>
               </div>
               <p>A modern version of "I-Spy", a hidden object game application. Created in HTML, CSS, and vanilla JavaScript within a 4-day sprint. </p>
             </li>
